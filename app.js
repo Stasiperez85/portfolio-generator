85c2d93288da786e1a1b5a58bdb4047c32cae9d0
Anastasia Perez <Stasiperez85@gmail.com>
2022-02-08 21:40:34 -0700

    finish adding new files and code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const { resourceUsage } = require('process');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
 
 
@@ -134,15 +134,23 @@ return inquirer.prompt([
       }
     });
   };
-    promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
   
-      fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw new Error(err);
-  
-        console.log('Page created! Check out index.html in this directory to see it!');
-      });
-    });
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
     
